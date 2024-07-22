bef31be7f3ec0e55f91002e8158560f39ddd0347
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-22 22:41:34 +0300

    1. add 'stream().findFirst().orElse()'

--- src/main/java/AleksLiss/LearningJavaCore/stream/pipelineMethods/FindFirstStream.java
+++ src/main/java/AleksLiss/LearningJavaCore/stream/pipelineMethods/FindFirstStream.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FindFirstStream {
+
     public static void main(String[] args) {
         List<Integer> list = getList();
         List<Integer> emptyList = new ArrayList<>();
