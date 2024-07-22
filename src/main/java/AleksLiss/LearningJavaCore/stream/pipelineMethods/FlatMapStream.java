18d10d0c4bc067dd85c7ad61b4fb10c18636469d
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-22 21:28:23 +0300

    1. add 'stream().flatMap()'

--- src/main/java/AleksLiss/LearningJavaCore/stream/pipelineMethods/FlatMapStream.java
+++ src/main/java/AleksLiss/LearningJavaCore/stream/pipelineMethods/FlatMapStream.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlatMapStream {
+
     public static void main(String[] args) {
         List<Integer> listOne = getListOne();
         List<Integer> listTwo = getListTwo();
