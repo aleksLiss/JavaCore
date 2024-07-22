d4049aa7e33918b8ffbaf369b2e29fb3ec6b3370
aleksLiss <darklisik.darklisik@yandex.ru>
2024-07-22 23:27:03 +0300

    1. add 'stream().sum()'

---
+++ src/main/java/AleksLiss/LearningJavaCore/stream/terminalMethods/agregationFunc/SumFunc.java
@@ -0,0 +1,18 @@
+package AleksLiss.LearningJavaCore.stream.terminalMethods.agregationFunc;
+
+import java.util.List;
+import java.util.Optional;
+
+public class SumFunc {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("Joka", 23),
+                new Person("Boka", 12),
+                new Person("Koka", 8)
+        );
+        Integer sumAges = people.stream()
+                .mapToInt(Person::age)
+                .sum();
+        System.out.println(sumAges);
+    }
+}
